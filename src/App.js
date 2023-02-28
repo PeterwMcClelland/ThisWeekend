9ea70d76db61f6a488c57692bb449c0112678445
Peter McClelland <peterwmcclelland@gmail.com>
2023-02-28 16:24:47 -0600

    login framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from "./components/homepage/Homepage"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route
 
+} from "react-router-dom";
+import {useState} from 'react';
 function App() {
+  const [user,setLoginUser] = useState({
+
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+<Switch>
+  <Route exact path="/">
+    {
+      user && user._id ? <Homepage/>:<Login/>
+    }<Homepage/></Route>
+  <Route path="/Login"><Login setLoginUser={setLoginUser}/></Route>
+  <Route path="/Register"><Register/></Route>
+</Switch>
+
+      </Router>
+
     </div>
   );
 }
