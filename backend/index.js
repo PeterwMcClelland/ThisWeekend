b1c31ad83b10d606736bc6af94997584d629f204
Peter McClelland <peterwmcclelland@gmail.com>
2023-02-28 22:27:07 -0600

    backend debuging

--- backend/index.js
+++ backend/index.js
@@ -1,67 +1,27 @@
-import express from "express";
-// const express = require("express");
-import cors from "cors";
-import mongoose from "mongoose";
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+//import routes
+const authRoutes = require('./routes/auth');
+const { db } = require('./models/User');
+//app
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
+// db
+mongoose
+ .connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+ .then(() => console.log('DB Connected'));
+
+//middlewares
+app.use(bodyParser.json());
 app.use(cors());
-
-
-mongoose.connect("mongodb://localhost:27017/auth",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-});()=>{
-    console.log("connected to DB")
-}
-
-
-//user schema 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
-
-const User = new mongoose.model("User", userSchema)
-
-//routes routes
-app.post("/Login",(req,res)=>{
-    const {email,password} =req.body;
-    User.findone({email:email},(err,user)=>{
-        if(user){
-           if(password === user.password){
-               res.send({message:"login sucess",user:user})
-           }else{
-               res.send({message:"wrong credentials"})
-           }
-        }else{
-            res.send("not register")
-        }
-    })
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
 });
-app.post("/Register",(req,res)=>{
-    console.log(req.body) 
-    const {name,email,password} =req.body;
-    User.findOne({email:email},(err,user)=>{
-        if(user){
-            res.send({message:"user already exist"})
-        }else {
-            const user = new User({name,email,password})
-            user.save(err=>{
-                if(err){
-                    res.send(err)
-                }else{
-                    res.send({message:"sucessfull"})
-                }
-            })
-        }
-    })
-
-
-}) 
-
-app.listen(3000,()=>{
-    console.log("started")
-})
